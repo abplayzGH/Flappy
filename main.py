d1dd622d4aeedd6ef6e3c019b63212202de744d4
Adam Lawrence <adam@impostertwo.com>
2024-12-06 18:04:17 -0600

    pipes

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import random
 # Set up the screen
 wn = turtle.Screen()
 wn.title("Flappy")
-wn.bgcolor("white")
-wn.setup(width=800, height=600)
+wn.bgcolor("lightblue")
+wn.setup(width=500, height=400)
+wn.register_shape('pipetop.gif')
+wn.register_shape('pipebottom.gif')
 
 # Create a turtle
 player = turtle.Turtle()
@@ -26,15 +28,46 @@ def go_down():
     y -= 20
     player.sety(y)
 
-def generate_pipe():
+def generate_pipe_top():
     pipe = turtle.Turtle()
-    pipe.shape("square")
-    pipe.color("red")
+    pipe.hideturtle()
+    pipe.shape("pipetop.gif")
     pipe.penup()
     pipe.speed(0)
-    pipe.goto(400, random.randint(-200, 200))
-
+    pipe.goto(300, random.randint(100, 200))
+    pipe.setheading(180)
+    pipe.showturtle()
     return pipe
+
+def generate_pipe_bottom():
+    pipe = turtle.Turtle()
+    pipe.hideturtle()
+    pipe.shape("pipebottom.gif")
+    pipe.penup()
+    pipe.speed(0)
+    pipe.goto(300, random.randint(-200, -100))
+    pipe.setheading(180)
+    pipe.showturtle()
+    return pipe
+
+pipes = []
+pipe_speed = 2
+
+def create_pipes():
+    pipes.append(generate_pipe_top())
+    pipes.append(generate_pipe_bottom())
+    
+def move_pipe():
+    if pipes[0].xcor() < -300:
+        for i in range (len(pipes)):
+            pipes[i].clear()
+            pipes[i].hideturtle()
+        pipes.clear()
+        create_pipes()
+    else:
+        for item in range (len(pipes)):
+            pipes[item].forward(pipe_speed)
+
 
 # Keyboard bindings
 wn.listen()
@@ -42,5 +75,7 @@ wn.onkey(go_up, "Up")
 wn.onkey(go_down, "Down")
 
 # Main game loop
+create_pipes()
 while True:
+    move_pipe()
     wn.update()
