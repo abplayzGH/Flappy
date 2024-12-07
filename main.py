ae8c8b0ba7b15e4960bbf3331ae8b0908eb3d620
Adam Lawrence <adam@impostertwo.com>
2024-12-06 18:21:01 -0600

    Implement game over functionality and adjust gravity mechanics

--- main.py
+++ main.py
@@ -18,16 +18,33 @@ player.penup()
 player.speed(0)
 player.goto(0, 0)
 
+#variables
+
+game_over_var = False
+pipes = []
+pipe_speed = 2
+
+# Functions
+
 def go_up():
     y = player.ycor()
-    y += 20
+    y += 30
     player.sety(y)
 
-def go_down():
+def gravity():
     y = player.ycor()
-    y -= 20
+    y -= 2
     player.sety(y)
+    if player.ycor() <= -200 or player.ycor() >= 200:
+        game_over()
 
+def game_over():
+    global game_over_var
+    player.hideturtle()
+    player.goto(0, 0)
+    player.write("Game Over", align="center", font=("Courier", 24, "normal"))
+    game_over_var = True
+    
 def generate_pipe_top():
     pipe = turtle.Turtle()
     pipe.hideturtle()
@@ -50,32 +67,35 @@ def generate_pipe_bottom():
     pipe.showturtle()
     return pipe
 
-pipes = []
-pipe_speed = 2
-
 def create_pipes():
     pipes.append(generate_pipe_top())
     pipes.append(generate_pipe_bottom())
     
 def move_pipe():
-    if pipes[0].xcor() < -300:
-        for i in range (len(pipes)):
-            pipes[i].clear()
-            pipes[i].hideturtle()
-        pipes.clear()
-        create_pipes()
+    global game_over_var
+    if game_over_var:
+        return None
     else:
-        for item in range (len(pipes)):
-            pipes[item].forward(pipe_speed)
+        if pipes[0].xcor() < -300:
+            for i in range (len(pipes)):
+                pipes[i].clear()
+                pipes[i].hideturtle()
+            pipes.clear()
+            create_pipes()
+        else:
+            for item in range (len(pipes)):
+                pipes[item].forward(pipe_speed)
 
 
 # Keyboard bindings
 wn.listen()
 wn.onkey(go_up, "Up")
-wn.onkey(go_down, "Down")
+wn.onkey(gravity, "Down")
 
 # Main game loop
 create_pipes()
 while True:
+    print(game_over_var)
+    gravity()
     move_pipe()
     wn.update()
